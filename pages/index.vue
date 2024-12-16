4834c47479abb50f997f93cdde94078f5495b0ea
Joseph <joseph.hagan@azoom.jp>
2024-12-16 19:48:13 +0900

    Set up iOS capacitor

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+<ion-page>
+    <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">Hello World</ion-content>
+  </ion-page>
+</ion-page>
+</template>
+<script lang="ts" setup>
+
+</script>
